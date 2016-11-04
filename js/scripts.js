2d9c77d4b2750995dcf2969b36285df570104023
Chris Clifford <cclifford82@gmail.com>
2016-11-04 14:43:18 -0700

    add jquery code to create pizza object

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,20 @@
 // Buisness Logic
 
 //constructors
-function Pizza(pizzaSize) {
-  this.pizzaSize = pizzaSize;
-  this.crustType;
-  this.sauceType;
-  this.vegToppings = [];
-  this.meatToppings = [];
-  this.cost;
+function Customer() {
+  this.order = [];
+  this.firstName;
+  this.lastNamet;
+  this.address = {};
 }
 
-function Customer() {
-  this.firstName = first;
-  this.lastName = last;
-  this.address = {};
+function Pizza(pizzaSize, crustType, sauceType, vegToppings, meatToppings) {
+  this.pizzaSize = pizzaSize;
+  this.crustType = crustType;
+  this.sauceType = sauceType;
+  this.vegToppings = vegToppings;
+  this.meatToppings = meatToppings;
+  this.cost;
 }
 
 function Address() {
@@ -55,8 +56,7 @@ Pizza.prototype.addToOrder = function() {
 
 //User Interface Logic
 $(document).ready(function() {
-
-  var newPizza = new Pizza()
+  var newCustomer = new Customer();
   // var newCustomer = new Customer()
   $("#order-now").click(function() {
     $(".home").hide();
@@ -64,33 +64,44 @@ $(document).ready(function() {
     $(".form-size").toggle();
   })
 
-  $(".form-size").submit(function(event) {
-    event.preventDefault();
-    var size = $("input:radio[name=size]:checked").val();
-    var newPizza = new Pizza(size);
+  $("#form-size").click(function() {
     $(".form-size").toggle();
     $(".form-crust").toggle();
-    console.log(newPizza);
   })
 
-  $(".form-crust").submit(function(event) {
-    event.preventDefault();
-    var crust = $("input:radio[name=crust]:checked").val();
-    newPizza.crust = crust;
+  $("#form-crust").click(function() {
     $(".form-crust").toggle();
     $(".form-sauce").toggle();
-    console.log(newPizza);
   })
 
-  $(".form-sauce").submit(function(event) {
-    event.preventDefault();
-    var sauce = $("input:radio[name=sauce]:checked").val();
-    newPizza.sauce = sauce;
+  $("#form-sauce").click(function() {
+    $(".form-sauce").toggle();
+    $(".form-vegTops").toggle();
+  })
+
+  $("#form-vegTops").click(function() {
     $(".form-vegTops").toggle();
     $(".form-meatTops").toggle();
-    console.log(newPizza);
   })
 
+  $(".form-pizza-order").submit(function(event) {
+    event.preventDefault();
+    var size = $("input:radio[name=size]:checked").val();
+    var crust = $("input:radio[name=crust]:checked").val();
+    var sauce = $("input:radio[name=sauce]:checked").val();
+    var vegToppings = [];
+    var meatToppings = [];
+    $("input:checkbox[name=veg-topping]:checked").each(function(){
+          vegToppings.push($(this).val())
+    })
+    $("input:checkbox[name=meat-topping]:checked").each(function(){
+          meatToppings.push($(this).val())
+    })
+    var newPizza = new Pizza(size, crust, sauce, vegToppings, meatToppings);
+    console.log(newPizza);
+    newCustomer.order.push(newPizza);
+    console.log(newCustomer);
+  })
 
 
 })
