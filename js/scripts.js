4cd3091284e276299779e46c853f14417f3eaa87
Chris Clifford <cclifford82@gmail.com>
2016-11-04 13:24:39 -0700

    added styling

--- js/scripts.js
+++ js/scripts.js
@@ -5,13 +5,14 @@ function Pizza(pizzaSize, crustType, sauceType) {
   this.pizzaSize = pizzaSize;
   this.crustType = crustType;
   this.sauceType = sauceType;
-  this.toppings = [];
+  this.vegToppings = [];
+  this.meatToppings = [];
 }
 
 function Customer() {
   this.firstName = first;
   this.lastName = last;
-  this.address = [];
+  this.address = {};
 }
 
 function Address() {
@@ -31,7 +32,10 @@ Pizza.prototype.getCost = function() {
   if (this.crustType === "Thick") {
     cost += 1;
   }
-  this.toppings.forEach(function(topping) {
+  this.vegToppings.forEach(function(topping) {
+    cost += 1;
+  })
+  this.meatToppings.forEach(function(topping) {
     cost += 2;
   })
   this.cost = cost;
@@ -48,7 +52,7 @@ Pizza.prototype.addToOrder = function() {
 
 var newPizza = new Pizza("Large", "Thick", "Red", "Extra")
 
-newPizza.toppings.push("pepperoni", "onions", "tomatoes")
+newPizza.vegToppings.push("pepperoni", "onions", "tomatoes")
 
 console.log(newPizza);
 newPizza.getCost();
@@ -58,7 +62,17 @@ console.log(newPizza.cost);
 
 //User Interface Logic
 $(document).ready(function() {
-  $("form").submit(function(ready) {
 
+  $("#order-now").click(function() {
+    $("#order-now").hide();
+    $(".panel").toggle();
   })
+
+  $("#order-now").submit(function(event) {
+    event.preventDefault();
+  })
+
+
+
+
 })
